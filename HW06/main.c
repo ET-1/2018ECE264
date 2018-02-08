ef09169be9211f45977a667f1d1de090f373629b
ET-1 <liu1493@purdue.edu>
2018-02-07 22:15:37 -0500

    First draft of main.c with segment fault

--- HW06/main.c
+++ HW06/main.c
@@ -11,7 +11,19 @@
 	4. Use `WriteDb()` to write to file named `output.txt`. You should hard code this name. 
 	5. Free memory.
  */
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
+    StudentDatabase * db = NULL;
+    ParseResult * res = NULL;
+    ExecuteResult * exe = NULL;
+    
+    db = Connect("database.txt");
+    res = ParseQuery(argc, argv);
+    exe = ExecuteQuery(db, res);
+    WriteDb(db, res->field, exe, "output.txt");
+    Close(db);
+    FreeParseResult(res);
+    FreeExecuteResult(exe);
     return EXIT_SUCCESS;
 }
 
