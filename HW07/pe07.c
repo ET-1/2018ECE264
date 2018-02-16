5c4bd947ffd90ca06b8abc3e98d294ea6edc8620
ET-1 <liu1493@purdue.edu>
2018-02-16 17:00:09 -0500

    Finish isint function and partial isdble function. isidentifier is still needed to be complete

--- HW07/pe07.c
+++ HW07/pe07.c
@@ -11,21 +11,68 @@
 #ifndef INT
 bool IsInteger(char* str, int index)
 {
-
-	
+  int nonint = 0;
+  index = 0;
+  for(int i = index; i < MAX_LENGTH; i++)
+  {
+    if(!isdigit(str[i]))
+    {
+      nonint ++;
+    }
+  }
+  if(nonint != 0)
+  {
+    return false;    
+  }
+  else
+  {
+    return true;
+  }
+  	
 }
 #endif
 
 #ifndef DOUBLE
-bool IsDouble(char* str, int index,bool dot)
+bool IsDouble(char* str, int index, bool dot)
 {
-	
+  int nondigit = 0;
+  int dott = 0;
+  int dot_idx = 0;
+  index = 0;
+  
+  for(int i = index; i < MAX_LENGTH; i++)
+  {
+    if(!isdigit(str[i]))
+    {
+      if(strcmp(str[i], ".") == 0)
+      {
+        dot_idx = i;
+        dot = true;        
+        dott ++;
+      }
+      else
+      {
+        nondigit ++;
+      }
+    }
+  }
+  
+  if((nondigit != 0) || (dott != 1) || (dot_idx == 0))
+  {
+    return true;
+  }
+  else
+  {
+    return false;    
+  }
+
 }
 #endif
 
 #ifndef VALID_ID
 bool IsValidIdentifier(char* str, int index)
 {
+  
 	
 }
 #endif
