714c9f1a4cdb668c30bab55d0b8069fd02734a05
ET-1 <liu1493@purdue.edu>
2018-02-22 16:59:01 -0500

    Add the comment for pe07.c file and improve the wrong operations in recursion

--- HW07/pe07.c
+++ HW07/pe07.c
@@ -12,15 +12,15 @@
 bool IsInteger(char* str, int index)
 {
   
-  if(*str == '\0')
+  if(*str == '\0')//End of the string
   {
     return true;//If there is no non-integer numbers, return true
   }
-  else
+  else//Not the end of the string
   {
     if(isdigit(*str))//If current character is a digit, do recursion
     {
-      return IsInteger(++str, ++index);//Go to check next character
+      return IsInteger(str + 1, index + 1);//Go to check next character
     }
     else
     {
@@ -34,44 +34,44 @@ bool IsInteger(char* str, int index)
 bool IsDouble(char* str, int index, bool dot)
 {
   int idx = 0;
-  while(str[idx] != '\0')
+  while(str[idx] != '\0')//Not the end of the string, go to next recursion
   {
     idx ++;
   }
   
-  if((*str == '\0') && (dot == true))
+  if((*str == '\0') && (dot == true))//End of the string and only one dot
   {
     return true;
   }
-  else
+  else//Not the end of the string
   {
-	  if(isdigit(*str))
+	  if(isdigit(*str))//If string is a digit
 	  {
-	    if(dot == false)
+	    if(dot == false)//If no dot before
 	    {
-	      return IsDouble(++str, ++index, false);
+	      return IsDouble(str + 1, index + 1, false);//Go to next recursion with dot is false
 	    }
-	    else
+	    else//If there is a dot before
 	    {
-	      return IsDouble(++str, ++index, true);	      
+	      return IsDouble(str + 1, index + 1, true);//Go to next recursion with dot is true	      
 	    }
 	  }	  
-	  else
+	  else//If string is not a digit
 	  {
-	    if(*str == '.')
+	    if(*str == '.')//If string is a dot
 	    {
-	      if((index != 0) && (dot != true) && (str[1] != '\0'))
+	      if((index != 0) && (dot != true) && (str[1] != '\0'))//If the dot is not at first and not at last and there is no dot before
 	      {
-	        return IsDouble(++str, ++index, true);
+	        return IsDouble(str + 1, index + 1, true);//Go to the next recursion with dot is true
 	      }
 	      else
 	      {
-	        return false;
+	        return false;//Break the last rule, not a double
 	      }
 	    }
 	    else
 	    {
-	      return false;
+	      return false;//Not a digit, not a double
 	    }
 	  }
 	}
@@ -83,38 +83,38 @@ bool IsDouble(char* str, int index, bool dot)
 #ifndef VALIDID
 bool IsValidIdentifier(char* str, int index)
 {
-  if(*str == '\0')
+  if(*str == '\0')//End of the string, it's a identifier
   {
     return true;
   }
-  else
+  else//Not at the end of the string
   {
-    if(isalpha(*str))
+    if(isalpha(*str))//If the string is not a  alphabe
     {
-      return IsValidIdentifier(++str, ++index);
+      return IsValidIdentifier(str + 1, index + 1);//Go to the next recursion
     }
     
     
-    else if(isdigit(*str))
+    else if(isdigit(*str))//If the string is not a digit
     {
-      if(index == 0)
+      if(index == 0)//If the first element is digit, not identifier
       {
         return false;
       }
-      else
+      else//Else go to the next recursion 
       {
-        return IsValidIdentifier(++str, ++index);
+        return IsValidIdentifier(str + 1, index + 1);
       }
     }
     
     
-    else if(*str == '_')
+    else if(*str == '_')//If the string is "_"
     {
-      return IsValidIdentifier(++str, ++index);
+      return IsValidIdentifier(str + 1, index + 1);//Go to next recursion
     }
     
     
-    else
+    else//If the string is not "_" or digit, not a identifier
     {
       return false;
     }
