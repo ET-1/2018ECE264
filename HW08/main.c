1bf7359c0c722c8f8fb3cafee014d335ae723965
ET-1 <liu1493@purdue.edu>
2018-02-22 22:35:37 -0500

    Finish the skeleton of the main.c file

--- HW08/main.c
+++ HW08/main.c
@@ -4,7 +4,39 @@
 #include "pa08.h"
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	DoubleVar * var;
+	int len = 0;
+	char * string;
+	var = malloc(sizeof(DoubleVar) * 1);
+	if(argc < 5)//If arguments are not sufficient
+	{
+	  return EXIT_FAILURE;//Return failure
+	} 
+	else//If arguments are sufficient
+	{
+	  if((argc == 5) && (strcmp(argv[1], "-D") == 0))
+	  {
+	    //1. Setup for the parameters extracted from argv
+	    var->digit_start = (int) strtol(argv [2], NULL, 10) ;
+	    var->digit_end = (int) strtol(argv [3], NULL, 10) ;
+	    len = (int) strtol(argv [4], NULL, 10) ;
+	    string = malloc(sizeof(char) * len);
+	    for(int i = 0; i < len; i++)
+	    {
+	      string[i] = 0;
+	    }
+	    
+	    //2. Excute the createDouble function for Double mode
+	    createDouble(string, var, 0, false, len);
+	    free(var);
+	    free(string);
+	  
+	  }
+	  //3. Excute the createVID function for VID mode
+	  
+	}
 	
 	return EXIT_SUCCESS;
 }
