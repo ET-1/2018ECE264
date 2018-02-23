9f45533664eb8f0e9c4798d3608054f2220a9ceb
ET-1 <liu1493@purdue.edu>
2018-02-22 22:36:27 -0500

    Finish the first draft of pa08.c. Realize some parts of requirements

--- HW08/pa08.c
+++ HW08/pa08.c
@@ -5,6 +5,42 @@
 #ifndef CDOUBLE
 void createDouble(char * string,DoubleVar* var,int index,bool dot,int len)
 {
+  int sel = 0;
+  int start = var->digit_start;
+  int end = var->digit_end;
+  if(index >= 2)
+  {
+    if(index < len)
+    {
+      for(int i = 0; i < len; i++)
+      {
+        printf("%c", string[i]);
+      }
+      printf(",");
+      
+    }
+      return;
+  }
+  for(sel = start; sel < (end + 1); sel++ )
+  {
+    if(dot == true)
+    {
+      string[index] = '.';
+    }
+    else
+    {
+      string[index] = sel + '0';
+    }
+    
+    if((index != (len - 1)) && (dot != true))
+    {
+      createDouble(string, var, index + 1, true, len);
+    }
+    else
+    {
+      createDouble(string, var, index + 1, false, len);
+    }
+  }
 
 		
 		
