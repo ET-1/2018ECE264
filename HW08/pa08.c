b982f58b6a725187f0612136de8fb6a3c12ec215
ET-1 <liu1493@purdue.edu>
2018-02-23 16:14:47 -0500

    Realize the dot function. But there are a lot of repeations of outputs.

--- HW08/pa08.c
+++ HW08/pa08.c
@@ -10,26 +10,48 @@ void createDouble(char * string,DoubleVar* var,int index,bool dot,int len)
   int end = var->digit_end;
     
     //Do the recursion starting from start to end
-    if(index == len)
+    if((index == len))
     {
-      //If the length of the string is larger than 3, print out each string  
       for(int i = 0; i < len; i++)
       {
-        printf("%c", string[i]);
+        if(string[i] == '.')
+        {
+          dot = true;
+        }
       }
-      printf("\n");
+      
+      //If the length of the string is larger than 3, print out each string 
+      if(dot == true)
+      { 
+        for(int i = 0; i < len; i++)
+        {
+          printf("%c", string[i]);
+        }
+        printf("\n");
+      }
       return;
     }
     else
-    {
-      for(sel = start; sel <= end; sel++)
+    { 
+      for(int pos = 1; pos <= (len -2); pos++)
       {
-        
-        string[index] = sel + '0';
-        createDouble(string, var, index + 1, false, len);
+        if((index == pos) && (dot != true))
+        {
+          string[index] = '.';
+          createDouble(string, var, index + 1, true, len);
+        }
+        else
+        {
+          for(sel = start; sel <= end; sel++)
+          {
+            string[index] = sel + '0';
+            //var->digit_start = var->digit_start + 1;
+            createDouble(string, var, index + 1, false, len);
+          }
+        }
       }
-      
     }
+      
 }
 
   
