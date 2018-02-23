3383f0d0ec126d27dda6a62158cca3bae1ddaee3
ET-1 <liu1493@purdue.edu>
2018-02-23 14:51:28 -0500

    Finish the number part and realize the recursion perfectly. Still need to realize the dot part.

--- HW08/pa08.c
+++ HW08/pa08.c
@@ -8,46 +8,32 @@ void createDouble(char * string,DoubleVar* var,int index,bool dot,int len)
   int sel = 0;
   int start = var->digit_start;
   int end = var->digit_end;
-  if(index >= 2)
-  {
-    if(index < len)
+    
+    //Do the recursion starting from start to end
+    if(index == len)
     {
+      //If the length of the string is larger than 3, print out each string  
       for(int i = 0; i < len; i++)
       {
         printf("%c", string[i]);
       }
-      printf(",");
+      printf("\n");
+      return;
+    }
+    else
+    {
+      for(sel = start; sel <= end; sel++)
+      {
+        
+        string[index] = sel + '0';
+        createDouble(string, var, index + 1, false, len);
+      }
       
     }
-      return;
-  }
-  for(sel = start; sel < (end + 1); sel++ )
-  {
-    if(dot == true)
-    {
-      string[index] = '.';
-    }
-    else
-    {
-      string[index] = sel + '0';
-    }
-    
-    if((index != (len - 1)) && (dot != true))
-    {
-      createDouble(string, var, index + 1, true, len);
-    }
-    else
-    {
-      createDouble(string, var, index + 1, false, len);
-    }
-  }
-
-		
-		
-	
-
-
 }
+
+  
+
 #endif
 
 #ifndef CVID
