887fed22f6e9660685c80a63cc6ef2df36e83def
ET-1 <liu1493@purdue.edu>
2018-03-03 19:28:59 -0500

    First draft of the read_write.c. Pass compiler. Pass no testcases.

--- HW09/read_write.c
+++ HW09/read_write.c
@@ -5,24 +5,60 @@
 
 #ifndef CONNECT
 
-StudentDatabase * ReadDatabase(char * filename) {
+StudentDatabase * ReadDatabase(char * filename) 
+{
 	StudentDatabase* db = NULL;
+	int count = 0;
+	int num_stu = 0;
+	Student * temp = NULL;
+	temp = malloc(sizeof(Student));
 	//TODO 1. Open the file in read and binary format
+  FILE * fptr;
+  fptr = fopen(filename, "rb");
+  if(fptr == NULL)//Fail to connect to database
+  {
+    printf("Fail to connect to db\n");
+  }
+  
+  else//Success to connect to database
+  {
+	  //TODO 2. Allocate memory for db
+    db = malloc(sizeof(StudentDatabase));
+  
+  
+	  //TODO 3. Find number to entries of Student structure
+    while((count = fread(temp, sizeof(Student), 1, fptr)) == 1)
+    {
+      num_stu = num_stu + 1;
+    }
+	
+	
+	  //TODO 4. Set the value of db->number to the number of students
+    db->number = num_stu;
 
-	//TODO 2. Allocate memory for db
-
-	//TODO 3. Find number to entries of Student structure
-
-	//TODO 4. Set the value of db->number to the number of students
-
-
-	// TODO 5. Allocate the memory for the db->students structure
-
-	//TODO 6. Read the file again from start and assign value to each student in the data base
-
+	
+	  // TODO 5. Allocate the memory for the db->students structure
+	  db->students = malloc(sizeof(Student) * (db->number));
+	
+    for(int i = 0; i < (db->number); i++)
+  	{
+ 	   db->students[i] = malloc(sizeof(Student));
+  	}
+  
+	
+		//TODO 6. Read the file again from start and assign value to each student in the data base
+ 	  fseek(fptr, 0, SEEK_SET);
+ 	  
+ 	  for(int j = 0; j < (db->number); j++)
+ 	  {
+ 	    fread(db->students[j], sizeof(Student), 1, fptr);
+ 	  }
+  
+	}
 	//close the file
-
-	//return the StudentDatabase
+  fclose(fptr);
+  free(temp);
+  //return the StudentDatabase
 	return db;
 }
 
@@ -30,12 +66,74 @@ StudentDatabase * ReadDatabase(char * filename) {
 
 #ifndef WRITE_DB
 //This function is to write to a file in binary format
-int WriteDatabase(char *filename, StudentDatabase * db) {
+int WriteDatabase(char *filename, StudentDatabase * db) 
+{
+  int count = 0;
+  FILE * fptr;
+  int write_flag = 0;
 	//TODO 1. Open the file in write and binary format
-	//TODO 2. check if the StudentDatabase is not null
-	//TODO 3. write into the file and if not possible return 0
-	return 1;
+	fptr = fopen(filename, "wb");
+	
+	if(fptr == NULL)//Fail to open file
+	{
+	  printf("Fail to open a file\n");
+	  return 0;
+	}
+	
+	else//Success to open file
+	{
+	
+		//TODO 2. check if the StudentDatabase is not null
+		if(db == NULL)//Database is empty
+		{
+	 	  printf("Empty database\n");
+	 	  fclose(fptr);
+	 	  return 0;
+		}
+		
+		
+		//TODO 3. write into the file and if not possible return 0
+		else//Database is not empty
+		{  
+		  for(int i = 0; i < (db->number); i++)//Write all student into file in binary 
+		  {
+		    write_flag = fwrite(db, sizeof(Student), 1, fptr);
+		    
+		    if(write_flag == 1)//Compare the written size
+		    {
+		      count ++;
+		    }
+		  }
+		  
+		  if(count == (db->number))//Check if total written size fit the students number 
+		  {
+		    fclose(fptr);
+		    return 1;
+		  }
+		  else
+		  {
+		    fclose(fptr);
+		    return 0;
+		  }
+		  
+		}
+	
+	}
 
 
 }
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
