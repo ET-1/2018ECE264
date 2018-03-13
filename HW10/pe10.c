8b941ea57e2606a2c5a90b2fe575686ed5f7804e
ET-1 <liu1493@purdue.edu>
2018-03-13 14:41:55 -0400

    Finished homework 10. Passed all testcases.

--- HW10/pe10.c
+++ HW10/pe10.c
@@ -16,6 +16,24 @@
 // 2- Make sure that there is a new line at the end of the output (i.e printf("\n")). 
 void print(bool * arr,int len)
 {
+  //Print out the array based on TRUE or FALSE. If it is TRUE, print its index. Otherwise, print 'X' instead.
+  for(int i = 0; i < len; i ++)
+  {
+		if(arr[i] == true)
+		{
+		  printf("%d", i);
+		}
+		else
+		{
+		  printf("X");
+		}
+		
+		if(i < len - 1)
+		{
+		  printf(",");
+		}
+  }
+	
 	
 	
 }
@@ -30,8 +48,62 @@ void print(bool * arr,int len)
 // Your function should stop when it has only one element left.
 void josephus(bool * arr, int len, int k,int p)
 {
+  int remain = len;
+  int idx = 0;
+  int count = 0;
+  
+  while(remain != 1)//If there are remaining number in the array
+  {
+    count = 0;//Reset the count for true number
+    
+    while(count < (k + 1) )//If true number count is smaller than skip number
+    {
+      if(  idx % len  == 0 && idx >= len )//Back to head of array if the index is out of range of array
+      {
+        idx = 0;
+      }
+      
+      
+      if(count == k )//If true number count equals skip number, set that number to false
+      {
+        while(arr[idx] != true)//If that number is already false, go to next true number 
+        {
+          idx ++;
+          
+          if(  idx % len  == 0 && idx >= len )
+      		{
+        		idx = 0;
+      		}
+        }
+        
+        arr[idx] = false;
+        remain --;
+        idx ++;
+        count ++;
+      }
 
-	
+      else if(arr[idx] == true && count != k)
+      {
+        idx ++;
+        count ++;
+      }
+      
+      else if(arr[idx] == false && count != k)
+      {
+        idx ++;
+      }      
+
+
+    }
+
+		  	
+  	if((remain % p) == 0)//Print out the array based on the divided number
+  	{
+  	  print(arr, len);
+  	  printf("\n");
+  	}
+  }
+	 
 
 }
 #endif
