9400734ed85381ad515d8e5ce96406e8cf93a8e4
ET-1 <liu1493@purdue.edu>
2018-03-13 14:41:46 -0400

    Finished homework 10. Passed all testcases.

--- HW10/main.c
+++ HW10/main.c
@@ -4,7 +4,33 @@
 #include "pe10.h"
 
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
+  int length = 0;
+  int skip = 0;
+  int divide = 0;
+  bool * arr = NULL;
+  
+  if(argc != 4)//If the input arguments are not 4, return FAILURE
+  {
+    return EXIT_FAILURE;
+  }
+  
+  else//Otherwise, excute josephus function
+  {
+    length = atoi(argv[1]);
+    skip = atoi(argv[2]);
+    divide = atoi(argv[3]);
+    arr = malloc(sizeof(bool) * length);
+    
+    for(int i = 0; i < length; i ++)
+    {
+      arr[i] = true;
+    }
+    
+    josephus(arr, length, skip, divide);
+    free(arr);    
+  }
 
 	
 	return EXIT_SUCCESS;
