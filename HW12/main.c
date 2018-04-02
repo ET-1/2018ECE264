7dcd3d7561487e9296c96dfdb43de3251d3b97b5
ET-1 <liu1493@purdue.edu>
2018-04-02 16:13:34 -0400

    Finish main.c for hw12. Pass all testcases. Do not finish the bonus part.

--- HW12/main.c
+++ HW12/main.c
@@ -8,5 +8,22 @@
 
 int main(int argc, char **argv)
 {
+	struct node * head = NULL;
+	if(argc != 5)
+	{
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		int length = atoi(argv[2]);
+		int k = atoi(argv[3]);
+		int p = atoi(argv[4]);
+		
+		if(strcmp(argv[1], "-J") == 0)
+		{
+			josp(&head, k, p, length);
+		}
+	}
 	
+	return EXIT_SUCCESS;
 }
