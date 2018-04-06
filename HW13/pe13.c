c8216b4705a4c0fe9b6625307d4a2ad73ea4cdb7
ET-1 <liu1493@purdue.edu>
2018-04-05 21:20:32 -0400

    Finish homework 13 pe13.c file. Pass all testcases.

--- HW13/pe13.c
+++ HW13/pe13.c
@@ -6,12 +6,46 @@
 // Step 4. - Test all the test cases 
 
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
   //0. Create array of CharFreq
+  CharFreq frequency[NUMLETTER];
+  char * filename = NULL;
+  int num_char = 0;
+	
+	if(argc != 2)
+	{
+		printf("Wrong arguments\n");
+		return EXIT_FAILURE;
+	}
+	else
+	{
   //1. initialize all the frequencies inside freq array to be 0
+		//frequency = malloc(sizeof(CharFreq)*NUMLETTER);
+		
+		for(int idx = 0; idx < NUMLETTER; idx ++)
+		{
+			frequency[idx].data = '0';		
+			frequency[idx].freq = 0;
+		}
+
   //2. Count Frequency from the the input file
+		filename = argv[1];
+  	num_char = CountFrequency(filename, frequency);
+  	
   //2a. if return value from the function is not success, return exit failure
+  	if(num_char == 0)
+  	{
+  		return EXIT_FAILURE;
+  	}
+  
   //3. Sort frequencies that you get
+  	SortFrequency(frequency);
+  
   //4. Print the frequencies on terminal
+  	PrintFreq(frequency);
+  	
+  }
+  //free(frequency);
   return EXIT_SUCCESS;
 }
