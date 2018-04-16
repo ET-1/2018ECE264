af5e85aec09ba146e4e52514ac5bc57e9abf62a3
ET-1 <liu1493@purdue.edu>
2018-04-16 16:15:26 -0400

    First draft of hw14. Pass part of the testcases.

--- HW14/pa14.c
+++ HW14/pa14.c
@@ -4,15 +4,55 @@
 
 #ifndef MAIN_FUNC
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
   // Create CharFreq array
-  // initialize all the frequencies inside freq array to be 0
-  // count frequency by calling CountFrequency function and passing the input filename
-  // sort frequencies by calling SortFrequency function
-  // build linked list sorted linked list
-  // Get two minimum frequencies and merge them and reduce one list node - call BuildHuffmanTree.
-  // Print the final huffman tree in prefix by calling the print function - call the prefix print function
-  // Free all the memory allocated
+  CharFreq frequency[NUMLETTER];
+  char * filename = NULL;
+  int num_char = 0;
+  
+	if(argc != 2)
+	{
+		printf("Wrong arguments\n");
+		return EXIT_FAILURE;
+	}
+	else
+	{		
+		// initialize all the frequencies inside freq array to be 0
+		for(int idx = 0; idx < NUMLETTER; idx ++)
+		{
+			frequency[idx].data = '0';		
+			frequency[idx].freq = 0;
+		}
+
+ 	  // count frequency by calling CountFrequency function and passing the input filename
+		filename = argv[1];
+  	num_char = CountFrequency(filename, frequency);
+  	
+  //2a. if return value from the function is not success, return exit failure
+  	if(num_char == 0)
+  	{
+  		printf("Wrong arguments\n");
+  		return EXIT_FAILURE;
+  	}
+  
+  	// sort frequencies by calling SortFrequency function
+  	SortFrequency(frequency);
+  	
+  	// build linked list sorted linked list
+  	ListNode * head = ListBuild(frequency);
+  	
+  	// Get two minimum frequencies and merge them and reduce one list node - call BuildHuffmanTree.
+  	head = BuildHuffmanTree(head);
+  	
+  	// Print the final huffman tree in prefix by calling the print function - call the prefix print function
+  	PrefixTreePrint(head->treeNodePtr, 1);
+  
+  	// Free all the memory allocated  
+  	CleanTree(head->treeNodePtr);
+  	CleanList(head);
+  	
+  }
 
   return EXIT_SUCCESS;
 }
